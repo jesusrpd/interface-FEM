ac3f73773b7008731fdef2702c03875a920aa9ea
unknown <jesusrodrigo881@gmail.com>
2024-11-26 22:28:28 -0600

    conected api and inputs base for the problem

--- app/page.js
+++ app/page.js
@@ -1,101 +1,58 @@
-import Image from "next/image";
+"use client"
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const submitData = async (e) => {
+    e.preventDefault();
+    console.log(e);
+    const dataFrom = {
+      a1: Number(e.target[0].value),
+      a2: Number(e.target[1].value),
+      l: Number(e.target[2].value),
+      p: Number(e.target[3].value),
+      t: Number(e.target[4].value),
+      e: Number(e.target[5].value),
+      n: Number(e.target[6].value),
+      p: Number(e.target[7].value)
+    }
+    console.log(dataFrom);
+    
+    const response = await fetch("http://127.0.0.1:8000/calculate", {
+      method: "POST",
+      body: JSON.stringify(dataFrom),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+
+    if(!response.ok){
+      throw new Error("Error al hacer el calculo")
+    }
+
+    const data = await response.json();
+    console.log(data)
+
+    console.log(response.body)
+  }
+
+  return (
+    <div>
+      <h2>Ingreso de datos:</h2>
+        <form onSubmit={submitData}>
+          <legend>
+            <div>
+              <label>a1: <input type="text" placeholder="a1"/></label>
+              <label>a2: <input type="text" placeholder="a2"/></label>
+              <label>l: <input type="text" placeholder="l"/></label>
+              <label>p: <input type="text" placeholder="p"/></label>
+              <label>t: <input type="text" placeholder="t"/></label>
+              <label>E: <input type="text" placeholder="E"/></label>
+              <label>Número de nodos: <input type="text" placeholder="N"/></label>
+              <label>En que nodo va tu carga: <input type="text" placeholder="?"/></label>
+            </div>
+          </legend>
+          <button type="submit">Calcular</button>
+        </form>
     </div>
   );
 }
